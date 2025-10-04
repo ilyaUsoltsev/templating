56b2766881e0995bb49515b974998109285d6b52
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2025-10-04 21:16:41 +0200

    Make lib

--- src/app.ts
+++ src/app.ts
@@ -3,52 +3,24 @@ import path from 'path';
 import { Scanner } from './scanner';
 import Parser from './parser';
 import AstPrinter from './ast-printer';
-import { Stmt } from './ast';
 
-const partials = {} as { [key: string]: Stmt[] };
+const partials = {};
 
-const partialFileNames = [
-  'Dialog.handlebars',
-  'Button.handlebars',
-  'Container.handlebars',
-];
-
-for (const fileName of partialFileNames) {
-  const partialPath = path.join(__dirname, fileName);
-  const partialFileContent = fs.readFileSync(partialPath, 'utf8');
-  const partialScanner = new Scanner(partialFileContent);
-  const partialTokens = partialScanner.scanTokens();
-  const partialParser = new Parser(partialTokens);
-  const partialStatements = partialParser.parse(partialTokens);
-  const key = path.basename(fileName, path.extname(fileName));
-  partials[key] = partialStatements;
+export function registerPartials(name: string, template: string) {
+  partials[name] = template;
 }
 
-const filePath = path.join(__dirname, 'test.handlebars');
-const fileContent = fs.readFileSync(filePath, 'utf8');
+export function compile(fileContent: string) {
+  const scanner = new Scanner(fileContent);
+  const tokens = scanner.scanTokens();
+  const parser = new Parser(tokens);
+  const statements = parser.parse(tokens);
+  const ast = new AstPrinter(partials);
 
-const scanner = new Scanner(fileContent);
-const tokens = scanner.scanTokens();
-for (const token of tokens) {
-  console.log(token.toString());
+  return (ctx: any) => ast.print(statements, ctx);
 }
 
-const parser = new Parser(tokens);
-const statements = parser.parse(tokens);
-
-const ast = new AstPrinter(partials);
-const printedAst = ast.print(statements, {
-  userId: 'user123',
-  greetingClass: 'grueziClass',
-  name: 'Peter',
-  isMember: true,
-  messages: [
-    { text: 'Text1', user: 'User1' },
-    { text: 'Text2', user: 'User2' },
-  ],
-});
-
-console.log('---------------------------');
-console.log(JSON.stringify(statements, null, 2));
-console.log('---------------------------');
-console.log(printedAst);
+export default {
+  compile,
+  registerPartials,
+};
